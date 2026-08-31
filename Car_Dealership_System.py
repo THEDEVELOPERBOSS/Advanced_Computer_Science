8f7c104c6016276ac4c30d5239f75d242f4920a8
Mark Dahl <ridges_busts.0a@icloud.com>
2026-08-31 13:33:11 -0600

    Working on Car dealership

---
+++ Car_Dealership_System.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    # Attributes:
+    def __init__(self, make, model, year, num_wheels, seating_capacity, top_speed):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.num_wheels = num_wheels
+        self.seating_capacity = seating_capacity
+        self.top_speed = top_speed
+
+def display_info(self.make, ):
+    print(f"It is a {self.make}")
+
+
+def add_car()
+
+def remove_car()
+
+def display_all()
+
+def main():
+    
