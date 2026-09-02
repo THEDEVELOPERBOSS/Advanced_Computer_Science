71f1c65e8b8b02b07c51bd755313c5036e5442fc
Mark Dahl <ridges_busts.0a@icloud.com>
2026-09-02 14:09:09 -0600

    Added base for inherting_methods.py

--- inheriting_methods.py
+++ inheriting_methods.py
@@ -4,4 +4,15 @@ class Vehicle:
         self.model = model
         self.top_speed = top_speed
         
-        
+    def start(self):
+        print(f"{self.make} {self.model} is starting.")
+    def drive(self):
+    
+    def stop(self):
+class Motorcycle(Vehicle):
+
+class Bus(Vehicle):
+    
+car = Vehicle("Porsche", "911", 183)
+m_cycle = Motorcycle("Kawasaki", "Ninja", 130)
+bus = Bus("Blue Bird", "Vision", 65)
