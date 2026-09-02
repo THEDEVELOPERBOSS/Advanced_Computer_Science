4fc2807f1cc5bf907a336b4cadf71ecf7a2e6dbf
Mark Dahl <ridges_busts.0a@icloud.com>
2026-09-02 13:07:37 -0600

    Finished display info

--- Car_Dealership_System.py
+++ Car_Dealership_System.py
@@ -11,22 +11,52 @@ class Vehicle:
 
         Vehicle.all_vehicles.append(self)
 
+# Prints all the info about the selected car
+def display_info(vehicle=None):
+    print("Available cars:")
+    for number, car in enumerate(Vehicle.all_vehicles, start=1):
+        print(f"{number}. {car.make} {car.model}")
 
-def display_info(self):
-    print(f"It is a {self.make} {self.model} from the year {self.year}. It has {self.num_wheels} of wheels. It can fit {self.seating_capacity} people. It has a top speed of {self.top_speed} mph.")
+    choice = int(input("Which car would you like to look at? "))
+    vehicle = Vehicle.all_vehicles[choice - 1] # uses - 1 because the list starts to index at 0 but the user will input one ahead of that 
+
+    print(f"It is a {vehicle.make} {vehicle.model} from the year {vehicle.year}. It has {vehicle.num_wheels} wheels. It can fit {vehicle.seating_capacity} people. It has a top speed of {vehicle.top_speed} mph.")
+    main()
 def add_car():
     print("Add car function")
+
+
 def remove_car():
     print("Remove car function")
+    
 def display_all():
     for vehicle in Vehicle.all_vehicles:
-        display_info(vehicle) 
+        display_info(vehicle)
+
 def main():
-    print("Main program")
+    print("\n1. Display info about a car")
+    print("2. Add a car to the inventory")
+    print("3. Remove a car from the inventory")
+    print("4. Display all the cars")
+    print("Q. Quit the program")
+    
+    user_input = input().strip()
+    if user_input == '1':
+        display_info()
+    elif user_input == '2':
+        add_car()
+    elif user_input == '3':
+        remove_car()
+    elif user_input == "4":
+        display_all()
+    elif user_input.lower() == 'q':
+        quit()
+    
 
 # Cars
 ford_raptor = Vehicle("Ford", "Raptor", 2026, 4, 5, 150)
 jeep_xj = Vehicle("Jeep", "Cheorkee", "2001", 4, 5, 100)
 
-display_info(ford_raptor)
-display_all()
+
+if __name__ == "__main__":
+    main()
