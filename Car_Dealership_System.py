d30136b63cd1897044defce3c5b2b1a4a30c982a
Mark Dahl <ridges_busts.0a@icloud.com>
2026-09-02 12:49:48 -0600

    Got display all function working

--- Car_Dealership_System.py
+++ Car_Dealership_System.py
@@ -1,5 +1,6 @@
 class Vehicle:
-    # Attributes:
+    all_vehicles = []
+
     def __init__(self, make, model, year, num_wheels, seating_capacity, top_speed):
         self.make = make
         self.model = model
@@ -8,15 +9,24 @@ class Vehicle:
         self.seating_capacity = seating_capacity
         self.top_speed = top_speed
 
-def display_info(self.make, ):
-    print(f"It is a {self.make}")
+        Vehicle.all_vehicles.append(self)
 
 
-def add_car()
-
-def remove_car()
-
-def display_all()
-
+def display_info(self):
+    print(f"It is a {self.make} {self.model} from the year {self.year}. It has {self.num_wheels} of wheels. It can fit {self.seating_capacity} people. It has a top speed of {self.top_speed} mph.")
+def add_car():
+    print("Add car function")
+def remove_car():
+    print("Remove car function")
+def display_all():
+    for vehicle in Vehicle.all_vehicles:
+        display_info(vehicle) 
 def main():
-    
+    print("Main program")
+
+# Cars
+ford_raptor = Vehicle("Ford", "Raptor", 2026, 4, 5, 150)
+jeep_xj = Vehicle("Jeep", "Cheorkee", "2001", 4, 5, 100)
+
+display_info(ford_raptor)
+display_all()
