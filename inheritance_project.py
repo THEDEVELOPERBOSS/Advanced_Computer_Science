fc4bd2b32a5bf0a539f774cac55d52bdce8eb121
Mark Dahl <ridges_busts.0a@icloud.com>
2026-09-03 16:29:08 -0600

    Started inheritance project

---
+++ inheritance_project.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    # attributes
+    def __init__(self, make, model, year, num_wheels, seating_capacity, top_speed):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.num_wheels = num_wheels
+        self.seating_capacity = seating_capacity
+        self.top_speed = top_speed
+        
+class 
