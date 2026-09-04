172da763107c950e5c9443d4595faa085f8d2afa
Mark Dahl <ridges_busts.0a@icloud.com>
2026-09-04 13:58:55 -0600

    started restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,20 @@
+class Restaurant:
+    def __init__(self, Name, Menu)
+    
+    def display_restaurant():
+    
+class Menu:
+    def __init__(self, food_options):
+        
+    def add_item():
+        
+    def display_menu():
+        
+class MenuItem:
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+    
+    def display_item(self):
+        print(f"{self.name} is a {self.category}. It costs $ {self.price}")
