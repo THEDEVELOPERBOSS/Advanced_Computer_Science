126365ead47f4c96d047166c9e447ef85d7c16b0
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-09-21 10:12:31 -0600

    finsihed test_add_item_and_size()

--- Pytest/shopping_cart/test_shopping_cart.py
+++ Pytest/shopping_cart/test_shopping_cart.py
@@ -1,4 +1,7 @@
 from shopping_cart import ShoppingCart
 
 def test_add_item_and_size():
-    
+    cart = ShoppingCart([]) # Creates the list
+    cart.add("Apple") # Adds apple to the list
+# remember create object → use object → check result   
+    assert cart.size() == 1 # Checks to make sure the cart contains one item
