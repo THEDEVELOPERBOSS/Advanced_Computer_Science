9a9d2287103b8f5853f6fe1aaccb8e48a6143ae8
MK_dahl08 <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-09-22 09:29:10 -0600

    figuring out rent function and pytest for it

--- Pytest/car_dealer/car_dealer.py
+++ Pytest/car_dealer/car_dealer.py
@@ -1,1 +1,11 @@
-print("Car dealer program")
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model 
+        self.year = year
+    
+    def rent(self):
+        if car_rent in self.cars_in_inventory:
+            return("Car has been taken out succesfully")
+        else:
+            return("Car is not in inventory")
--- Pytest/car_dealer/test_car_dealer.py
+++ Pytest/car_dealer/test_car_dealer.py
@@ -1,1 +1,6 @@
-print("Car dealer program")
+from car_dealer import Car
+
+def test_rent():
+    car_rented = Car("Ford", "Fiesta", 2005)
+    
+    assert car_rented.rent()
