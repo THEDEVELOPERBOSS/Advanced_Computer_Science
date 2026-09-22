32880a9d815b94ba70667adb639c0310b3acdc8a
MK_dahl08 <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-09-21 18:59:13 -0600

    finished test_remove_item_success

--- Pytest/shopping_cart/test_shopping_cart.py
+++ Pytest/shopping_cart/test_shopping_cart.py
@@ -5,3 +5,13 @@ def test_add_item_and_size():
     cart.add("Apple") # Adds apple to the list
 # remember create object → use object → check result   
     assert cart.size() == 1 # Checks to make sure the cart contains one item
+    
+def test_remove_item_succes():
+    cart = ShoppingCart([]) # Creates the list
+    cart.add("Banana") # Adds banana to the list
+    assert cart.remove("Banana") == True # Removes banana from the list
+    assert "Banana" not in cart.items
+    assert cart.size() == 0 # Checks to make sure the cart contains zero items
+    
+    
+    
