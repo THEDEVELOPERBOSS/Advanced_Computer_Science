e34a27192ba36f1c18252b22233f49ea0e17b785
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-09-24 19:36:48 -0600

    finished car_dealer pytest

--- Pytest/car_dealer/car_dealer.py
+++ Pytest/car_dealer/car_dealer.py
@@ -1,32 +1,38 @@
-class Car_dealer():
-    def __init__(self, make, model, year, car_rented):
+class Car_dealer(): # defines car class
+    def __init__(self, make, model, year, car_rented): 
         self.make = make
         self.model = model
         self.year = year
         self.rented = car_rented
 
-    def rent(self):
-        if self in cars_in_inventory:
-            self.rented = True
+    def rent(self): # rent function
+        if self in cars_in_inventory: # checks to see if the desired car is in inventory
+            self.rented = True # changes the status to be rented
             return "Car has been taken out succesfully"
-        else:
+        else: # what happens if the car is not in the inventory
             return "Car is not in inventory"
 
     def return_car(self):
-        if self.rented and self not in cars_in_inventory:
+        if self.rented and self not in cars_in_inventory: # checks to make the car being returned is not in inventory
             self.rented = False
-            cars_in_inventory.append(self)
+            cars_in_inventory.append(self) # puts it back into the inventory
             return "Car has been returned succesfully"
-        else:
+        else: # what happens if car is in inventory
             return "Car is still in inventory"
 
-    def return_wrong_car(self):
-        if self.rented and self not in master_inventory:
+    def return_wrong_car(self): # if someone returns a car that isn't one of the companies
+        if self.rented and self not in master_inventory: # checks to see if it is in master list
             return "This is not one of our cars. You are returning it to the wrong place"
         else:
             return "This is one of our cars"
-        
+    
+def add_car(self): # adding car to inventory
+    cars_in_inventory.append(self) # adds to list of cars not rented
+    master_inventory.append(self) # adds to master list of all cars
+    return master_inventory and cars_in_inventory
+
+new_car = Car_dealer("BLANK", "BLANK", 0000, True) # blank car
 Ford_fiesta = Car_dealer("Ford", "Fiesta", 2005, False)
 jeep_xj = Car_dealer("Jeep", "Cheorkee", 2001, True)
-cars_in_inventory = [Ford_fiesta]
-master_inventory = [Ford_fiesta, jeep_xj]
+cars_in_inventory = [Ford_fiesta] # cars not rented
+master_inventory = [Ford_fiesta, jeep_xj] # master list of all cars company owns
--- Pytest/car_dealer/test_car_dealer.py
+++ Pytest/car_dealer/test_car_dealer.py
@@ -1,25 +1,37 @@
-from car_dealer import Car_dealer, cars_in_inventory
+from car_dealer import Car_dealer, cars_in_inventory, master_inventory
 
-def test_rent():
-    car_rented = Car_dealer("Ford", "Fiesta", 2005, False)
-    cars_in_inventory.append(car_rented)
+def test_rent(): 
+    car_rented = Car_dealer("Ford", "Fiesta", 2005, False) # desired car
+    cars_in_inventory.append(car_rented) # takes it out 
 
-    assert car_rented.rent() == "Car has been taken out succesfully"
+    assert car_rented.rent() == "Car has been taken out succesfully" # makes sure it worked
     
-def test_rent_car_out():
-    car_rented = Car_dealer("Jeep", "Cheorkee", 2001, True)
+def test_rent_car_out(): # tests to see if you can rent an already rented car
+    car_rented = Car_dealer("Jeep", "Cheorkee", 2001, True) 
     
     assert car_rented.rent() == "Car is not in inventory"
 
-def test_return_car():
+def test_return_car(): # returning car
     car_rented = Car_dealer("Jeep", "Cheorkee", 2001, True)
-    
-    assert car_rented.return_car() == "Car has been returned succesfully"
 
-def test_return_car_in():
-    car_rented = Car_dealer("Ford", "Fiesta", 2005, False)
+    assert car_rented.return_car() == "Car has been returned succesfully"
+    assert car_rented.rented == False # changes status to not being rented
+    assert car_rented in cars_in_inventory # makes sure it is in invnentory again
+
+def test_return_car_in(): # tests returning a car that is already returned
+    car_rented = Car_dealer("Ford", "Fiesta", 2005, False) 
     cars_in_inventory.append(car_rented)
 
-def test_return_wrong_car():
-    car_rented = Car_dealer("Subaru", "Outback", 2010, True)
+    assert car_rented.return_car() == "Car is still in inventory"
+
+def test_return_wrong_car(): # returning a car that does not belong to them
+    car_rented = Car_dealer("Subaru", "Outback", 2010, True) # creates other car
     assert car_rented.return_wrong_car() == "This is not one of our cars. You are returning it to the wrong place"
+
+def test_add_car(): # adding a car
+    new_car = Car_dealer("Ford", "Mustang", 2020, False) # defines new car
+
+    new_car.add_car() # adds it with funciton
+
+    assert new_car in cars_in_inventory # checks to make sure it is in both inventorys
+    assert new_car in master_inventory
